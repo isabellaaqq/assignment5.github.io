f2207e216ab494bc8215389cf72cc0c957e035ca
isabellaaqq <71214721+isabellaaqq@users.noreply.github.com>
2020-11-18 06:53:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var e = document.getElementById("E");
+		a.addEventListener("click", a_OnClick);
+
+		function a_OnClick() {
+			var body = document.getElementById("body");
+			body.style.backgroundColor = "#45823e";
+		}
